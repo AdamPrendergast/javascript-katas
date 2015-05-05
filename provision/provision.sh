f1ae695dc353c143078faddb747f7797780e7e48
Adam Prendergast <adamprendergast@adams-mbp.lan>
2015-05-05 19:55:47 +0100

    Updated provision file

--- provision/provision.sh
+++ provision/provision.sh
@@ -2,7 +2,6 @@
 
 sudo apt-get update
 mkdir /vagrant/src
-mkdir /vagrant/src/test
 
 # Set SSH default directory
-echo "cd /vagrant/src/test" >> /home/vagrant/.bashrc
+echo "cd /vagrant/src" >> /home/vagrant/.bashrc
